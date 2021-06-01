a00005e023e3cac29ca69d731fe8c1812f7fec10
Seshadri-7 <84894346+Seshadri-7@users.noreply.github.com>
2021-06-01 12:08:56 +0530

    Update App.js

--- Part 1/1.1/App.js
+++ Part 1/1.1/App.js
@@ -3,11 +3,11 @@ import React from 'react'
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
+  const part1 = 'Fundamental of React'
   const exercises1 = 10
-  const part2 = 'Using props to pass data'
+  const part2 = 'Using props to pass data used'
   const exercises2 = 7
-  const part3 = 'State of a component'
+  const part3 = 'States of a component'
   const exercises3 = 14
 
   return (
